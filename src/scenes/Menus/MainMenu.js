6818ab505f3f3d1b43b45bdb4bf95ba32a25804f
Johnny <jmnaze123@gmail.com>
2024-10-09 19:02:20 -0400

    Create MainMenu.js
    Basic main menu for launching the game

---
+++ src/scenes/Menus/MainMenu.js
@@ -0,0 +1,58 @@
+import { Scene } from "phaser";
+
+export class MainMenu extends Scene {
+  constructor() {
+    super("MainMenu");
+
+    this.fontFamily = "sans-serif";
+    this.rem = 16;
+  }
+
+  create() {
+    // main menu text
+    this.add
+      .text(512, 200, "Shermm-er", {
+        fontFamily: this.fontFamily,
+        fontStyle: "bold",
+        fontSize: this.rem * 4 + "px",
+      })
+      .setOrigin(1 / 2);
+
+    // create a play button
+    const playButton = this.add
+      .text(512, 400, "Play", {
+        fontFamily: this.fontFamily,
+        fontStyle: "bold",
+        fontSize: this.rem * 2 + "px",
+        padding: { x: 100, y: 20 },
+
+        backgroundColor: "#3388FF",
+      })
+      .setOrigin(1 / 2)
+      .setInteractive({ useHandCursor: true });
+
+    const levelSelectButton = this.add
+      .text(512, 500, "Level Select", {
+        fontFamily: this.fontFamily,
+        fontStyle: "bold",
+        fontSize: this.rem * 2 + "px",
+        padding: {x: 100, y: 20 },
+
+        backgroundColor: "#3388FF",
+      })
+      .setOrigin(1 / 2)
+      .setInteractive({ useHandCursor: true });
+
+    // listeners
+
+    // play button on click
+    playButton.on("pointerdown", () => {
+      alert("play the highest available level");
+    });
+
+    // level select button on click
+    levelSelectButton.on("pointerdown", () => {
+      this.scene.start("LevelMenu");
+    });
+  }
+}
