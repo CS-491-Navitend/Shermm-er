26a07d755595c963f63b084dad2e2624a977564f
Johnny <jmnaze123@gmail.com>
2024-10-09 19:02:03 -0400

    Create Boot.js
    Boot scene to preload all assets for the main menu (for future use)

---
+++ src/scenes/Boot.js
@@ -0,0 +1,16 @@
+import { Scene } from "phaser";
+
+export class Boot extends Scene {
+  constructor() {
+    super("Boot");
+  }
+
+  preload() {
+    //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
+    //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
+  }
+
+  create() {
+    this.scene.start("MainMenu");
+  }
+}
