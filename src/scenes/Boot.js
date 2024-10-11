c86dbf25852f28cfe409c877e701e42812411353
Johnny <jmnaze123@gmail.com>
2024-10-11 10:44:00 -0400

    Update Boot.js
    new asset paths

--- src/scenes/Boot.js
+++ src/scenes/Boot.js
@@ -6,8 +6,6 @@ export class Boot extends Scene {
   }
 
   preload() {
-    //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
-    //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
     //Load vehicle images
     this.load.image("car1", "/assets/car1.png");
     this.load.image("car1forward", "/assets/car1forward.png");
@@ -18,7 +16,8 @@ export class Boot extends Scene {
     this.load.image("car3", "/assets/car3.png");
     this.load.image("car3forward", "/assets/car3forward.png");
 
-    this.load.image("tractor", "/assets/TractorTrailerForward.png");
+    this.load.image("tractor", "/assets/TractorTrailer.png");
+    this.load.image("tractorforward", "/assets/TractorTrailerforward.png");
 
     //Load images
     this.load.image("shermie", "/assets/shermie.png");
