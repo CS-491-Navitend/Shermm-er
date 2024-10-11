13c232a417687d56e88a7254b113a7702e9b8b3a
Steven Curran <s.curran244@gmail.com>
2024-10-11 13:13:12 -0400

    added setSize line

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ const config = {
   physics: {
     default: "arcade",
     arcade: {
-      debug: false, //handy to show collision
+      debug: true, //handy to show collision
     },
   },
   scale: {
