6c8e440b173ed31f5ebc8b3249121af2f4429b71
Bigglylate <cab1999626@gmail.com>
2024-11-25 20:56:21 -0500

    changed gameWin
    make random shermie assets to spawn in different locations

--- src/scenes/GameWin.js
+++ src/scenes/GameWin.js
@@ -12,11 +12,14 @@ export class GameWin extends Scene {
     
     const gameLogic = new GameLogic(data["game"]);
     // Display win message
-    this.add.text(data["game"].width / 2, 250, "You Win!", { fontSize: "32px", fill: "#fff" }).setOrigin(0.5);
+      const winText = this.add.text(data["game"].width / 2, 250, "You Win!", {
+          fontSize: "32px",
+          fill: "#fff",
+          fontFamily: "Pixel"
+      }).setOrigin(0.5);
 
-    // Display win count
-    this.add.text(data["game"].width / 2, 300, `Wins: ${data["game"].winCount}`, { fontSize: "32px", fill: "#fff" }).setOrigin(0.5);
-
+      winText.setDepth(10);
+    this.createRandomAssets(data["game"]);
     // Display resets count
     // this.add.text(data["game"].width / 2, 350, `Resets: ${data["game"].resetCount}`, { fontSize: "32px", fill: "#fff" }).setOrigin(0.5);
 
@@ -35,5 +38,17 @@ export class GameWin extends Scene {
     this.input.keyboard.on("keydown-M", () => {
       this.scene.start("MainMenu");
     });
+  }
+  createRandomAssets(gameData){
+      const assetCount = 20;
+
+      for(let i = 0; i < assetCount; i++) {
+          const x = Phaser.Math.Between(0, gameData.width);
+          const y = Phaser.Math.Between(0, gameData.height);
+
+          const assetChoice = ["shermie", "shermieBlue", "shermieGreen","shermieOrange", "shermiePurple","shermieRed", "shermieYellow"][Phaser.Math.Between(0, 6)];
+
+          this.add.image(x,y, assetChoice)
+      }
   }
 }
